64f2e592ee9224ac371b0d9ff7bfdf6a76adda90
Thuandang297 <Thuandang729@gmail.com>
2021-10-21 23:00:30 +0700

    first commit

---
+++ LearnJS/main.js
@@ -0,0 +1,19 @@
+// var animals=["Dog","Chicken","Duck","Fish"];
+var animals=["Cat"];
+animals.unshift("Elephant");
+// if(animals.length==0)
+// {
+//     animals.push("Cat","Mouse");
+
+// }
+//    if(animals.length==1)
+// {
+//     animals.shift("Elephant");
+
+// }
+//    if(animals.length>2)
+// {
+//     animals.splice(2,1);
+
+// }
+console.log(animals);
